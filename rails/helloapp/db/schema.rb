c16b91777f959c7b83952967de680808258802bc
Pavlo Zahozhenko <pavlozahozhenko@gmail.com>
2020-03-11 13:57:01 +0200

    ActiveRecord associations & validations

--- rails/helloapp/db/schema.rb
+++ rails/helloapp/db/schema.rb
@@ -10,11 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_104451) do
+ActiveRecord::Schema.define(version: 2020_03_11_104953) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "courses_students", id: false, force: :cascade do |t|
+    t.integer "course_id", null: false
+    t.integer "student_id", null: false
+  end
+
+  create_table "faculties", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "student_record_books", force: :cascade do |t|
+    t.float "avg_grade", default: 0.0, null: false
+    t.integer "student_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "name", limit: 100, default: "", null: false
     t.integer "age"
+    t.integer "faculty_id"
   end
 
 end
