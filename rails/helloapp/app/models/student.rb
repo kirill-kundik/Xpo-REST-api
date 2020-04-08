ce33c2621e7e13b4595fead727556472029071c6
Pavlo Zahozhenko <pavlozahozhenko@gmail.com>
2020-04-08 18:07:07 +0300

    Added rspec example

--- rails/helloapp/app/models/student.rb
+++ rails/helloapp/app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ApplicationRecord
   has_one :record_book, class_name: 'Student::RecordBook'
   belongs_to :faculty
   has_and_belongs_to_many :courses
+
+  def take_exam(course, grade = 0)
+    grade > 0 ? 61 : nil
+  end
 end
